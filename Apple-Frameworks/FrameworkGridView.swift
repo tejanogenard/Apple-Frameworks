f4b9ef3af74c104160e82629941c9890f1f0d994
Ivan Tejano <tejanogenard@gmail.com>
2024-08-07 12:51:02 -0700

    created images and stacked them in grid view

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -7,18 +7,32 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FrameworkGridView: View {
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Apple FrameWorks")
+        }
+        HStack{
+            Image(.appClip)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100,
+                       height: 100)
+            Image(.arkit)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100,
+                       height: 100)
+
+
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    FrameworkGridView()
 }
